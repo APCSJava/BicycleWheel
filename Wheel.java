8ffa904fedcf6697eae8f967e9bbae5f2b6e9da9
kentcollins <kent.r.collins@gmail.com>
2015-11-05 14:57:21 +0000

    updating wheel

--- Wheel.java
+++ Wheel.java
@@ -1,4 +1,20 @@
 
-public class Wheel {
+public class Wheel implements DataGenerator {
+	
+	double angularVelocity, wheelRadius, angle;
+	
+	public Wheel(double vel, double rad, double ang) {
+		angularVelocity = vel;
+		wheelRadius = rad;
+		angle = ang;
+	}
+
+	@Override
+	public void generateData(String outputFile, double time, double step) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }

04353223843e28cc0ebf28ec35d6a732762b1ee0
kentcollins <kent.r.collins@gmail.com>
2015-11-05 14:35:31 +0000

    initial commit

---
+++ SimulationRunner.java
@@ -0,0 +1,18 @@
+/**
+ * Creates and runs simulations on classes that can be simulated
+ * 
+ * @author kentc14
+ *
+ */
+public class SimulationRunner {
+
+	public static void main(String[] args) {
+		// modify the code below to call an appropriate constructor for your class
+		// DataGenerator wheel = new Wheel(???);
+		
+		// generate data for a specified time with constant delta-t
+		// wheel.generateData("wheel-data.csv", 2.0, 0.01);
+
+	}
+
+}
